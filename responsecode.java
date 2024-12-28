bb9d055d59caa5690458eb35356291b3b1d7ef98
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2024-12-28 09:14:26 +0530

    Add files via upload

---
+++ responsecode.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner S=new Scanner(System.in);
+		int x=S.nextInt();
+		if(x == 404)
+		{
+		    System.out.println("NOT FOUND");
+		}
+		else{
+		    System.out.println("FOUND");
+		}
+
+	}
+}
